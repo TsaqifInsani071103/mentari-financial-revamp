070ff3f26ce9fa6b83479d3034b1d54f63596883
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2024-05-16 05:37:04 -0700

    first EXPOSURE TO TDD INTEGRATED INTO MAVENgit status

---
+++ src/main/java/com/mentarirvmp/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.mentarirvmp.calculator;
+
+public class Calculator {
+  public Calculator(){
+
+  } 
+
+  public int add(int x, int y){
+    System.out.println("oopsie");
+    return 0; 
+  } 
+  
+}
