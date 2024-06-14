311e50ed5d788c17f194785bfa391cce26c9e478
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2024-06-13 22:11:04 -0700

    made some progress with formula object

--- src/main/java/com/mentarirvmp/utils/Formula.java
+++ src/main/java/com/mentarirvmp/utils/Formula.java
@@ -20,5 +20,27 @@ public class Formula {
     return this.formula; 
   } 
 
-  
+  public String getEmptyFormula(){
+    //we assume that all formulas has () for it to be a valid formula 
+    int startIndex = this.formula.indexOf('(');
+    int endIndex = this.formula.indexOf(')', startIndex);
+    if(validFormulaFormat(startIndex,endIndex)){
+      return parseIntoEmptyFormula(startIndex, endIndex);
+    }else{
+      return "invalid";
+    }
+  } 
+
+  private boolean validFormulaFormat(int startIndex, int endIndex){
+    startIndex = this.formula.indexOf('(');
+    endIndex = this.formula.indexOf(')', startIndex);
+    if(startIndex != -1 && endIndex !=-1) return true; 
+    return false; 
+  } 
+
+  private String parseIntoEmptyFormula(int startIndex, int endIndex){
+    String emptyString = formula.substring(0, startIndex + 1) + formula.substring(endIndex); 
+      return emptyString;
+  } 
+
 }
--- src/test/java/com/mentarirvmp/utils/FormulaTest.java
+++ src/test/java/com/mentarirvmp/utils/FormulaTest.java
@@ -5,10 +5,25 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FormulaTest {
+  private String dummyFormulaString = "=SUM()";
+  private Formula dummyFormulaObject = new Formula(dummyFormulaString);
   
   @Test 
   public void getFormulaTest(){
-    assertEquals(new Formula("=SUM()").getFormula(), "=SUM()");
+    assertEquals(this.dummyFormulaObject.getFormula(), this.dummyFormulaString);
   } 
+
+  @Test 
+  public void getEmptyFormula(){
+    //testing that the default =SUM() formula emptied out is still =SUM() 
+    assertEquals(dummyFormulaObject.getEmptyFormula(), dummyFormulaString);
+
+    //testing that if the Formula doesn't have the parantheses(), it should return with "invalid" 
+    assertEquals(new Formula("d").getEmptyFormula(), "invalid");
+
+    //testing that if there are nested formulas we would catch that too 
+    // assertEquals(new Formula());
+    
+  }
 
 }
