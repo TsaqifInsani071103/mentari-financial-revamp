85d198c411f45723c0a2a47915b0f1e5c3721c74
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2024-08-10 20:16:38 -0700

    deleted validFormulaOperations hashmap, still working on isFormulaValid()

--- src/main/java/com/mentarirvmp/utils/Formula.java
+++ src/main/java/com/mentarirvmp/utils/Formula.java
@@ -17,61 +17,43 @@ import java.util.HashMap;
 //formula is basically just a tool for StatementsAndExpensehandler to use to basically calculate the valus of the formula strings in the expenses 
 public class Formula {
 
-  //hardcoding valid formulas right now: make a MAP of this brodie 
-  private final static HashMap<String, String> validFormulaOperations = new HashMap<>();
-  //static setup operations 
-  static{
-    //change this to refer to methods later on instead of <String,String> you do <String, Method> or however you're supposed to do it 
-    validFormulaOperations.put("SUM()", "valid");
-    validFormulaOperations.put("MULTIPLY()", "valid");
+  // //hardcoding valid formulas right now: make a MAP of this brodie 
+  // private final static HashMap<String, String> validFormulaOperations = new HashMap<>();
+  // //static setup operations 
+  // static{
+  //   //change this to refer to methods later on instead of <String,String> you do <String, Method> or however you're supposed to do it 
+  //   validFormulaOperations.put("SUM()", "valid");
+  //   validFormulaOperations.put("MULTIPLY()", "valid");
+  // } 
+
+  public static boolean isFormulaValid(String formula){
+    String parentFormula = getParentFormulaIfStructureValid(formula);
+
+    if(parentFormula == "") return false; 
+    return true; 
+  }
+
+  private static String getParentFormulaIfStructureValid(String formula){
+    int lastIndex = formula.length()-1; 
+    int openingParanthesisIndex = formula.indexOf('(');
+    if(formula.charAt(lastIndex) != ')' || openingParanthesisIndex == -1) return "";
+
+    String formulaBehindOpenParanthesis = formula.substring(0, openingParanthesisIndex);
+    if(!recognizedFormula(formulaBehindOpenParanthesis)) return "";
+    return formulaBehindOpenParanthesis;
   } 
   
 
   //this is practically mutating the nodes so instead of just checking, we'll just get rootNode if formula is valid, else, nothing. 
-  //get formulaStructure if all formulas are valid. 
-  public static boolean isFormulaValid(String formula){
-    //base case 
-    //check if the formula can be split into array elements by using split(",") and then iterate through that. 
-    int lastIndex = formula.length()-1; 
-    int openingParanthesisIndex = formula.indexOf('(');
-    if(formula.charAt(lastIndex) != ')') return false; 
-    if(openingParanthesisIndex == -1) return false; 
 
-    String formulaBehindOpenParanthesis = formula.substring(0, openingParanthesisIndex);
-    if(!recognizedFormula(formulaBehindOpenParanthesis)) return false; 
-
-    //put the formulaBehindOpenParanthesis into a parent node in the tree. 
-    FormulaNode parentNode = new FormulaNode(formulaBehindOpenParanthesis);
-    String nestedContent = formula.substring(openingParanthesisIndex + 1, lastIndex);
-
-    // if(!isNestedFormulaValid(parentNode, nestedContent)) return false;
-
+  private static boolean isInteger(String str){
+    try{
+      Integer.parseInt(str);
+    }catch(Error e){
+      return false; 
+    }
     return true; 
   } 
-
-  private static boolean isNestedFormulaValid(FormulaNode parentFormula, String formula){
-    //base case 
-    if(formula.equals("")){ 
-      parentFormula.addChild(new FormulaNode("0"));
-      return true; 
-    }
-
-    String[] splitFormula = formula.split(",");
-    for(String nestedFormula:splitFormula){
-      try{
-        //check if its a number or a formula, if its a string its invalid 
-        int number = Integer.parseInt(nestedFormula);
-        System.out.println(number);
-        parentFormula.addChild(new FormulaNode(nestedFormula)); //add the number as is
-      }catch(Error e){
-        parentFormula.addChild(new FormulaNode(nestedFormula));
-        return isFormulaValid(nestedFormula); 
-      }
-    }
-
-    return false; 
-
-  }
 
   private static boolean recognizedFormula(String formula){
     switch(formula){
