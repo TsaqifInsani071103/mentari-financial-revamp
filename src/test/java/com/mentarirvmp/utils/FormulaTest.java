7403db27e35f5df7930a3a7c6cefa4e16525184e
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2024-08-10 23:12:33 -0700

    uncommented out some tests

--- src/test/java/com/mentarirvmp/utils/FormulaTest.java
+++ src/test/java/com/mentarirvmp/utils/FormulaTest.java
@@ -22,25 +22,25 @@ public class FormulaTest {
     //remember that you need to check for a recognized formula preceeding the opening paranthesis. 
     // String dummyFormula = "SUM(MULTIPLY(12, 32, SUM(32,32)), SUM(32, 43,SUM(3,3)))"; 
 
-    String dummyFormula = "SUM(MULTIPLY(1,2), SUM(1,2,3), MULTIPLY(1))";
+    String dummyFormula = "SUM(MULTIPLY(1,2,,), SUM(1,2,3), MULTIPLY(1))";
     //first case SUM(MULTIPLY()) is true  
-    assertEquals(true, Formula.isFormulaValid2(dummyFormula));
+    assertEquals(true, Formula.isFormulaValid(dummyFormula));
     
-    // //second case SUM)( is false 
-    // String dummyFormula2= "SUM)(";
-    // assertEquals(false, Formula.isFormulaValid(dummyFormula2));
+    //second case SUM)( is false 
+    String dummyFormula2= "SUM)(";
+    assertEquals(false, Formula.isFormulaValid(dummyFormula2));
 
-    // //third case SUM) is false 
-    // String dummyFormula3 = "SUM)";
-    // assertEquals(false, Formula.isFormulaValid(dummyFormula3));
+    //third case SUM) is false 
+    String dummyFormula3 = "SUM)";
+    assertEquals(false, Formula.isFormulaValid(dummyFormula3));
 
-    // //fourth case DUMMY() is false 
-    // String dummyFormula4 = "DUMMY()";
-    // assertEquals(false, Formula.isFormulaValid(dummyFormula4));
+    //fourth case DUMMY() is false 
+    String dummyFormula4 = "DUMMY()";
+    assertEquals(false, Formula.isFormulaValid(dummyFormula4));
 
-    // //fifth case SUM(MULTIPLY(), SUM()) is false 
-    // String dummyFormula5 = "SUM(MULTIPLY(1), SUM(1,2,3)"; 
-    // assertEquals(false, Formula.isFormulaValid(dummyFormula5));
+    //fifth case SUM(MULTIPLY(), SUM()) is false 
+    String dummyFormula5 = "SUM(MULTIPLY(1), SUM(1,2,3)"; 
+    assertEquals(false, Formula.isFormulaValid(dummyFormula5));
     
     
 
