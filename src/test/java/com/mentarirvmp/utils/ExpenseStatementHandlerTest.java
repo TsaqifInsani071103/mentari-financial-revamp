7875a64c6a453e3bc2c8aadd821fffff0a48ae90
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2024-11-28 15:50:32 -0800

    added tests for ExpenseStatementHandler.java

---
+++ src/test/java/com/mentarirvmp/utils/ExpenseStatementHandlerTest.java
@@ -0,0 +1,21 @@
+package com.mentarirvmp.utils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import com.mentarirvmp.statements.Statement;
+
+public class ExpenseStatementHandlerTest {
+  
+  @Test
+  public void getValueByIdTest(){
+    Statement dummyStatement = MockObjects.getDummyStatementObject();
+    ExpenseStatementHandler dataHandler = new ExpenseStatementHandler(dummyStatement);
+
+    for(Expenses expense: MockObjects.getAllContentIntoArray(dummyStatement)){
+      assertEquals(expense.getValue(), dataHandler.getValueById(expense.getId()));
+    }
+
+    assertNull(dataHandler.getValueById("awdafdgsfdew"));
+  } 
+
+}
